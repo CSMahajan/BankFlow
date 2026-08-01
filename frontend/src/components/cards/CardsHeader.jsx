7c9b3cd39d962c7c063a46dd55b8ca4a3c560113
Chaitanya Mahajan <chaitanya.mahajan13@gmail.com>
2026-08-01 20:12:36 +0530

    extracted cards as reusabled components

---
+++ frontend/src/components/cards/CardsHeader.jsx
@@ -0,0 +1,28 @@
+const CardsHeader = () => {
+    return (
+        <div style={{ marginBottom: "24px" }}>
+            <h2
+                style={{
+                    margin: 0,
+                    fontSize: "28px",
+                    fontWeight: "700",
+                    color: "#111827",
+                }}
+            >
+                💳 My Cards
+            </h2>
+
+            <p
+                style={{
+                    marginTop: "6px",
+                    color: "#6b7280",
+                    fontSize: "15px",
+                }}
+            >
+                Manage your debit and credit cards securely.
+            </p>
+        </div>
+    );
+};
+
+export default CardsHeader;

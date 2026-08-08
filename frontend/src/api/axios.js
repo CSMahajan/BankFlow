6ff12866cbfb0dc5110a8ca46124341ad0fa5cff
Chaitanya Mahajan <chaitanya.mahajan13@gmail.com>
2026-08-08 09:52:16 +0530

    axios api calling change

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,10 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "/api/v1",
-  headers: {
-    "Content-Type": "application/json",
-  },
+  baseURL: import.meta.env.VITE_API_URL || "/api/v1",
 });
 
 API.interceptors.request.use(

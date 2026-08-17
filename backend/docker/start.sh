5e328dcc2d59d776775eb7bedbb861ae5659d9d7
Chaitanya Mahajan <chaitanya.mahajan13@gmail.com>
2026-08-17 17:28:58 +0530

    clam av added in docker file

--- backend/docker/start.sh
+++ backend/docker/start.sh
@@ -3,26 +3,27 @@ set -e
 
 echo "Updating ClamAV database..."
 
-freshclam || echo "Freshclam failed, continuing..."
+freshclam || echo "Freshclam failed, continuing with existing database..."
 
-echo "Configuring ClamAV TCP..."
+echo "Configuring ClamAV..."
 
 sed -i 's/^LocalSocket/#LocalSocket/' /etc/clamav/clamd.conf
 
-echo "TCPSocket 3310" >> /etc/clamav/clamd.conf
-echo "TCPAddr 127.0.0.1" >> /etc/clamav/clamd.conf
+cat >> /etc/clamav/clamd.conf <<EOF
 
+TCPSocket 3310
+TCPAddr 127.0.0.1
+EOF
 
 echo "Starting ClamAV..."
 
 clamd &
 
-
 echo "Waiting for ClamAV..."
 
-for i in {1..30}
+for i in {1..60}
 do
-    if nc -z localhost 3310
+    if nc -z 127.0.0.1 3310
     then
         echo "ClamAV is ready"
         break
@@ -32,11 +33,10 @@ do
     sleep 2
 done
 
-
 echo "Starting Spring Boot..."
 
 exec java \
 -XX:+UseContainerSupport \
--XX:MaxRAMPercentage=75.0 \
+-XX:MaxRAMPercentage=45.0 \
 -Djava.security.egd=file:/dev/./urandom \
 -jar app.jar
